4193d8b52b41f1037d27a9d345d678d4547130a7
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-08 02:05:53 +0000

    Done with Collections chapter

--- server/seeder.js
+++ server/seeder.js
@@ -1,22 +1,17 @@
-var postsData = [
-  {
+if (Posts.find().count() === 0) {
+  Posts.insert({
     title: 'Open Source .NET: Finally',
     author: 'Andrew Brassington',
     url: 'http://www.brassington.io/microsoft/open-source-net-finally'
-  },
-  {
+  });
+  Posts.insert({
     title: 'Technical Interviews',
     author: 'Andrew Brassington',
     url: 'http://www.brassington.io/bootcamp/code-interviews'
-  },
-  {
+  });
+  Posts.insert({
     title: 'Wordpress to Jekyll: Almost Done',
     author: 'Andrew Brassington',
     url: 'http://www.brassington.io/blog-maintenence/migration-wordpress-to-jekylls'
-  }
-  ];
-Template.postsList.helpers({
-  posts: function () {
-    return Posts.find();
-  }
-});
+  });
+}
