32568077a7a40c81ab3eac1e73b3e24f97d22465
Andrew Brassington <andrewbrassington@Andrews-Mac.local>
2014-04-20 11:33:25 -0700

    Added accounts and template for header

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,3 @@
-var postsData = [
-  {
-    title: 'Open Source .NET: Finally',
-    author: 'Andrew Brassington',
-    url: 'http://www.brassington.io/microsoft/open-source-net-finally'
-  },
-  {
-    title: 'Technical Interviews',
-    author: 'Andrew Brassington',
-    url: 'http://www.brassington.io/bootcamp/code-interviews'
-  },
-  {
-    title: 'Wordpress to Jekyll: Almost Done',
-    author: 'Andrew Brassington',
-    url: 'http://www.brassington.io/blog-maintenence/migration-wordpress-to-jekylls'
-  }
-  ];
 Template.postsList.helpers({
   posts: function () {
     return Posts.find();
