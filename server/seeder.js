5ad874da3c8045050e04ea977aa722adae6f4b0d
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-20 20:06:03 -0700

    Finished through chapter 10.5

--- server/seeder.js
+++ server/seeder.js
@@ -1,17 +1,51 @@
 if (Posts.find().count() === 0) {
-  Posts.insert({
-    title: 'Open Source .NET: Finally',
-    author: 'Andrew Brassington',
-    url: 'http://www.brassington.io/microsoft/open-source-net-finally'
+  var now = new Date().getTime();
+
+  var andrewId = Meteor.users.insert({
+    profile: { name: 'Andrew Brassington' }
   });
+  var andrew = Meteor.users.findOne(andrewId);
+
+  var jacobId = Meteor.users.insert({
+    profile: { name: 'Jacob Brassington' }
+  });
+  var jacob = Meteor.users.findOne(jacobId);
+
+  var openSourceNet = Posts.insert({
+    title: 'Open Source .NET: Finally',
+    userId: andrew._id,
+    author: andrew.profile.name,
+    url: 'http://www.brassington.io/microsoft/open-source-net-finally',
+    submitted: now - 7 * 3600 * 1000,
+    commentsCount: 2
+  });
+
+  Comments.insert({
+    postId: openSourceNet,
+    userId: jacob._id,
+    author: jacob.profile.name,
+    submitted: now - 5 * 3600 * 1000,
+    body: 'Interesting insight Andrew, how can I get involved?'
+  });
+
+  Comments.insert({
+    postId: openSourceNet,
+    userId: andrew._id,
+    author: andrew.profile.name,
+    submitted: now - 5 * 3600 * 1000,
+    body: 'Just PM me and we\'ll get started right away!'
+  });
+
   Posts.insert({
     title: 'Technical Interviews',
     author: 'Andrew Brassington',
-    url: 'http://www.brassington.io/bootcamp/code-interviews'
+    url: 'http://www.brassington.io/bootcamp/code-interviews',
+    commentsCount: 0
   });
   Posts.insert({
     title: 'Wordpress to Jekyll: Almost Done',
     author: 'Andrew Brassington',
-    url: 'http://www.brassington.io/blog-maintenence/migration-wordpress-to-jekylls'
+    url: 'http://www.brassington.io/blog-maintenence/migration-wordpress-to-jekylls',
+    commentsCount: 0
   });
 }
